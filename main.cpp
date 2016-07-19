022f77dc439ed6a1a2e69dfd9826a1b1c718f900
pfent <philipp@fent.de>
2016-07-20 01:21:55 +0200

    fix memory leak with doc->create_page()

--- main.cpp
+++ main.cpp
@@ -9,32 +9,35 @@ using namespace std;
 
 auto getSpeiseplan() {
     const auto weekNumber = boost::posix_time::second_clock::local_time().date().week_number();
-    const string baseUrl{"http://www.betriebsrestaurant-gmbh.de/"};
-    const string index{"index.php?id=91"};
-    const regex pdfRegex{string{"<a href=\"(.*KW"} + to_string(weekNumber) + string{".+_FMI_.*\\.pdf)\" "}};
+    const auto baseUrl = string{"http://www.betriebsrestaurant-gmbh.de/"};
+    const auto index = string{"index.php?id=91"};
+    const auto pdfRegex = regex{string{"<a href=\"(.*KW"} + to_string(weekNumber) + string{".+_FMI_.*\\.pdf)\" "}};
 
-    vector<string> pdfs;
-
-    auto response = cpr::Get(cpr::Url{baseUrl + index});
-    if (response.status_code != 200) {
-        return string{};
-    }
-    stringstream stream{response.text};
-    for (string line; getline(stream, line);) {
-        smatch match;
-        if (regex_search(line, match, pdfRegex) && match.size() == 2) {
-            pdfs.push_back(match[1]);
+    const vector<string> pdfs = [baseUrl, index, pdfRegex]() {
+        auto res = vector<string>{};
+        auto response = cpr::Get(cpr::Url{baseUrl + index});
+        if (response.status_code != 200) {
+            return res;
         }
-    }
+        auto stream = stringstream{response.text};
+        for (string line; getline(stream, line);) {
+            smatch match;
+            if (regex_search(line, match, pdfRegex) && match.size() == 2) {
+                res.push_back(match[1]);
+            }
+        }
+        return res;
+    }();
 
-    string result;
+    auto result = string{};
     for (const auto &pdf : pdfs) {
-        response = cpr::Get(cpr::Url{baseUrl + pdf});
-        vector<char> bytes{response.text.begin(), response.text.end()};
-        unique_ptr<poppler::document> doc(poppler::document::load_from_data(&bytes));
+        const auto response = cpr::Get(cpr::Url{baseUrl + pdf});
+        auto bytes = vector<char>{response.text.begin(), response.text.end()};
+        auto doc = unique_ptr<poppler::document>(poppler::document::load_from_data(&bytes));
         const auto pages = doc->pages();
         for (int i = 0; i < pages; ++i) {
-            auto text = doc->create_page(i)->text().to_utf8();
+            const auto page = unique_ptr<poppler::page>(doc->create_page(i));
+            const auto text = page->text().to_utf8();
             result += string(text.begin(), text.end());
         }
     }
@@ -42,14 +45,14 @@ auto getSpeiseplan() {
 }
 
 auto parseMeals(const string &data) {
-    map<string, vector<pair<string, double>>> mealPlan;
-    stringstream stream(data);
-    string currentDay{};
-    vector<pair<string, double>> currentMeals{};
-    const regex daysRegex{"^[Montag|Dienstag|Mittwoch|Donnerstag|Freitag]"};
-    const regex mealRegex{"^\\d+\\.\\s*(.*) (\\d\\.\\d\\d)"};
+    auto mealPlan = map<string, vector<pair<string, double>>>{};
+    auto stream = stringstream{data};
+    auto currentDay = string{};
+    auto currentMeals = vector<pair<string, double>>{};
+    const auto daysRegex = regex{"^[Montag|Dienstag|Mittwoch|Donnerstag|Freitag]"};
+    const auto mealRegex = regex{"^\\d+\\.\\s*(.*) (\\d\\.\\d\\d)"};
     for (string line; getline(stream, line);) {
-        smatch match;
+        auto match = smatch{};
         if (regex_search(line, daysRegex)) {
             if (currentMeals.size() != 0) {
                 mealPlan[currentDay] = currentMeals;
@@ -72,19 +75,19 @@ int main(int, const char **) {
         return -1;
     }
     // c++ regex can't multiline match. [\s\S]* is the multiline match equivalent to .*
-    const regex header{"([\\s\\S]*Tagessuppe.*)"};
-    const regex footer{"(Enthält laut neuer Lebensmittel-Informationsverordnung:[\\s\\S]*)"};
+    const auto header = regex{"([\\s\\S]*Tagessuppe.*)"};
+    const auto footer = regex{"(Enthält laut neuer Lebensmittel-Informationsverordnung:[\\s\\S]*)"};
 
     raw = regex_replace(raw, header, "");
     raw = regex_replace(raw, footer, "");
     raw = regex_replace(raw, regex{"(\n )"}, " "); // some lines have random newlines in them
-    raw = regex_replace(raw, regex{"oder B.n.W."}, "");
-    raw = regex_replace(raw, regex{"\\*"}, "");
+    raw = regex_replace(raw, regex{"oder B.n.W."}, ""); // Beilage nach Wahl
+    raw = regex_replace(raw, regex{"\\*"}, ""); // random asterisks
+    raw = regex_replace(raw, regex{" \\d(,\\d)* "}, ""); // additives "1,2,3"
 
     raw = regex_replace(raw, regex{"( +)"}, " "); // remove superfluous whitespace
 
     auto mealPlan = parseMeals(raw);
-
     for (const auto &day : mealPlan) {
         cout << day.first << " has following meals:\n";
         for (const auto &meal : day.second) {
